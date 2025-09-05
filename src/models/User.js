1ce83a7408d024db46ed37567eb4e519731287c2
Amrit-raj50 <amrit.raj.cg@gmail.com>
2025-09-05 15:16:51 +0530

    Add email validation and user validation method

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,9 @@
 class User {
   constructor(name, email) {
     this.name = name;
-    this.email = email;
+    this.email = email.toLowerCase(); this.password = password;
   }
 }
+  validate() {
+    return this.name && this.email.includes('@');
+  }
